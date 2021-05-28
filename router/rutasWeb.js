a8c063de1709465fc531f276d23672ec474ead4d
unknown <zepedayimy46@gmail.com>
2021-05-28 18:18:39 +0200

    segundo commit

--- router/rutasWeb.js
+++ router/rutasWeb.js
@@ -7,5 +7,21 @@ router.get('/', (req, res) =>{
     res.render("index", {titulo:'Pagina de inicio'})
 })
 
+router.get('/nosotros', (req, res) =>{
+    res.render("nosotros", {titulo:'Pagina de Nosotros'})
+})
+
+router.get('/anuncios', (req, res) =>{
+    res.render("anuncios", {titulo:'Pagina de Anuncios'})
+})
+
+router.get('/blog', (req, res) =>{
+    res.render("blog", {titulo:'Pagina de Blog'})
+})
+
+router.get('/contacto', (req, res) =>{
+    res.render("contacto", {titulo:'Pagina de contacto'})
+})
+
 
 module.exports = router;
